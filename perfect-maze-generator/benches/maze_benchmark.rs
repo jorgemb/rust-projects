5ecd00584271f00ef2d5bb92a1d0033997356dcc
Jorge L. Martinez <jorelmb@gmail.com>
2023-06-16 20:49:17 +0200

    Adding benchmark support.

---
+++ perfect-maze-generator/benches/maze_benchmark.rs
@@ -0,0 +1,17 @@
+use criterion::{BenchmarkId, black_box, Criterion, criterion_group, criterion_main};
+use perfect_maze_generator::PerfectMaze;
+
+fn sized_maze(c: &mut Criterion) {
+    let seed = Some(42);
+    let mut group = c.benchmark_group("PerfectMaze");
+
+    for size in [10, 20, 50] {
+        group.bench_with_input(BenchmarkId::from_parameter(size),
+                               &size, |b, &size| {
+                b.iter(|| PerfectMaze::new(size, size, seed));
+            });
+    }
+}
+
+criterion_group!(benches, sized_maze);
+criterion_main!(benches);
