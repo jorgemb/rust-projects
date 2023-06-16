7dcb060c0dacdc64fff4e625d07614a44dec07f0
Jorge L. Martinez <jorelmb@gmail.com>
2023-06-16 10:21:46 +0200

    Maze working with user input.

--- perfect-maze-generator/src/main.rs
+++ perfect-maze-generator/src/main.rs
@@ -1,6 +1,34 @@
 use perfect_maze_generator as maze_generator;
+use std::io;
+use std::io::prelude::*;
+use std::str;
+
+/// Asks for input from the user
+fn ask_input<T>(prompt: &str) -> Option<T>
+    where
+        T: str::FromStr {
+    // Show prompt and read next line
+    println!("{prompt}");
+
+    // Read line from user
+    let mut line = String::new();
+    io::stdin().read_line(&mut line).expect("Error reading input");
+
+    let value: Result<T, _> = line.trim().parse::<T>();
+    match value {
+        Ok(v) => Some(v),
+        Err(_) => None
+    }
+}
 
 fn main() {
-    let maze = maze_generator::PerfectMaze::new(2, 3, Some(0));
+    println!("Welcome to Perfect Maze Generator. Answer the following questions");
+    println!("to generate a new maze.");
+
+    // Ask the user for input
+    let rows = ask_input::<usize>("Rows? ").unwrap();
+    let columns = ask_input::<usize>("Columns? ").unwrap();
+
+    let maze = maze_generator::PerfectMaze::new(columns, rows, None);
     println!("{maze}");
 }
